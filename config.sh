1d9af8a84a4a28c30a232000763a6481688120c0
John Angel <john.angel@tdstelecom.com>
2016-03-15 14:43:22 -0500

    corrected ip address

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ set -e
 set -x
  
 sysctl kernel.hostname="ipaserver.domain.local"
-SERVER_IP_ADDR=192.168.19.15
+SERVER_IP_ADDR=192.168.56.101
 SERVER_FQDN=`hostname`
 SERVER_NAME=`hostname | cut -d. -f 1 | tr '[:upper:]' '[:lower:]'`
 IPA_REALM=EXAMPLE.COM
