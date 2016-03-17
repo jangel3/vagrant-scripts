3b97f2eb3c4bf847ee5e7c95f3875a6e5b2e6fbd
John Angel <jangel3@gmail.com>
2016-03-16 23:09:20 -0500

    corrected command to set hostname.

--- config.sh
+++ config.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
  
-sysctl kernel.hostname="ipaserver.domain.local"
-SERVER_IP_ADDR=192.168.56.104
+hostnamectl --static set-hostname "ipaserver.domain.local"
+SERVER_IP_ADDR=192.168.56.101
 SERVER_FQDN=`hostname`
 SERVER_NAME=`hostname | cut -d. -f 1 | tr '[:upper:]' '[:lower:]'`
 IPA_REALM=EXAMPLE.COM
