d3b4aaafd75429b3859d0236083ae93ccad07e28
John Angel <john.angel@tdstelecom.com>
2016-03-23 14:19:31 -0500

    FQDN and ip address was incorrect.

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ hostnamectl --static set-hostname "ipaserver.domain.local"
 SERVER_IP_ADDR=192.168.56.101
 SERVER_FQDN=`hostname`
 SERVER_NAME=`hostname | cut -d. -f 1 | tr '[:upper:]' '[:lower:]'`
-IPA_REALM=EXAMPLE.COM
-IPA_DOMAIN=example.com
+IPA_REALM=DOMAIN.LOCAL
+IPA_DOMAIN=domain.local
 FORWARDER=10.0.2.3
 PASSWORD=aaaAAA111
